c1b38bac1e46e2c7687927ee3d8b9310015bf13a
ovinzhou <www.274512594@qq.com>
2018-06-14 16:43:42 +0800

    modified:   sub.py

--- sub.py
+++ sub.py
@@ -1,12 +1,12 @@
 import requests
 import random
 from lxml import etree
+import re
 
 
 class Opensubzm(object):
     def __init__(self):
-        self.base_url = 'https://www.opensubtitles.org/en/search2/sublanguageid-all/moviename-'
-        user_agent_list = [
+        self.user_agent_list = [
             "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
             "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)",
             "Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
@@ -23,35 +23,65 @@ class Opensubzm(object):
             "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
             "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20",
             "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36 OPR/26.0.1656.60",
+            "Opera/8.0 (Windows NT 5.1; U; en)",
+            "Mozilla/5.0 (Windows NT 5.1; U; en; rv:1.8.1) Gecko/20061208 Firefox/2.0.0 Opera 9.50",
+            "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; en) Opera 9.50",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0",
+            "Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.10) Gecko/20100922 Ubuntu/10.10 (maverick) Firefox/3.6.10",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.57.2 (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36",
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11",
+            "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.16 (KHTML, like Gecko) Chrome/10.0.648.133 Safari/534.16",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.11 TaoBrowser/2.0 Safari/536.11",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71 Safari/537.1 LBBROWSER",
+            "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; LBBROWSER) ",
+            "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E; LBBROWSER)",
+            "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; QQBrowser/7.0.3698.400)",
+            "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)",
+            "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.84 Safari/535.11 SE 2.X MetaSr 1.0",
+            "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SV1; QQDownload 732; .NET4.0C; .NET4.0E; SE 2.X MetaSr 1.0)",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Maxthon/4.4.3.4000 Chrome/30.0.1599.101 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.122 UBrowser/4.0.3214.0 Safari/537.36",
+            "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50",
+            "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
+            "Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
+            "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11",
+            "Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
         ]
         self.headers = {
-            'User-Agent': random.choice(user_agent_list)
+            'User-Agent': ''
         }
+        self.rex = r'/(\d*)/'
 
     def request(self, url):
+        self.headers['User-Agent'] = random.choice(self.user_agent_list)
         response = requests.get(url, headers=self.headers)
-        return response.text
+        return response.content
 
     def analysis(self, content):
         xdata = etree.HTML(content)
-        info_url = xdata.xpath('//*[contains(@id, name)]/strong/a/@href')[0]
-        info_url = 'https://www.opensubtitles.org' + info_url
-        response = self.request(info_url)
-        xdata = etree.HTML(response)
-        link_list = xdata.xpath('//*[contains(@id, name)]/td[5]/a/@href')
+        info_url_list = xdata.xpath('//*[contains(@id, name)]/strong/a/@href')
         country_list = xdata.xpath('//*[contains(@id, name)]/td[2]/a/div/@class')
-        del link_list[0]
-        for link, country in zip(link_list, country_list):
-            print(link)
-            print(country.replace('flag', '').strip())
+        for info_url, country in zip(info_url_list, country_list):
+            movie_id = re.findall(self.rex, info_url)[0]
+            self.headers['referer'] = 'https://www.opensubtitles.org' + info_url
+            url = 'https://dl.opensubtitles.org/en/download/sub/' + movie_id
+            response = self.request(url)
+            zm_path = './zm/' + movie_id + '.zip'
+            print(movie_id, country.replace('flag', '').strip(), zm_path)
+            with open(zm_path, 'wb') as f:
+                f.write(response)
 
     def save_data(self):
         pass
 
     def run(self):
-        film_name = input('请输入电影名称>>>')
-        url = self.base_url + film_name
-        print(url)
+        url = 'https://www.opensubtitles.org/en/search/sublanguageid-all/moviename-a'
         response = self.request(url)
         self.analysis(response)
 
@@ -59,8 +89,3 @@ class Opensubzm(object):
 if __name__ == '__main__':
     osz = Opensubzm()
     osz.run()
-
-# 国家 //*[contains(@id, name)]/td[2]/a/div/@class
-# Captain America: Civil War
-# black panther
-# Avengers: Infinity War
